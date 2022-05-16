181c32b4a91c5b7fc95d305347cc04296c3e5510
Nazariy <44504451+naziktron@users.noreply.github.com>
2022-05-16 11:21:48 +0300

    finish1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+    const isDisabled = true;
+    let timeInterval = 0;
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+btnStop.disabled = isDisabled;
+
+btnStart.addEventListener('click' , onStartClick);
+btnStop.addEventListener('click' , onStopClick);
+
+function changeBodyBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStartClick() {
+    btnStart.disabled = isDisabled;
+    btnStop.disabled =  !isDisabled;
+    timeInterval = setInterval(changeBodyBgColor,1000);
+}
+
+function onStopClick() {
+    btnStart.disabled =  !isDisabled;
+    btnStop.disabled =  isDisabled;
+    clearInterval(timeInterval);
+}
